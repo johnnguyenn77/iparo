9ba9991c475c1c8e00fa2e40075938373e7ed776
Patrick <43623131+21pxle@users.noreply.github.com>
2025-05-11 08:57:42 -0400

    Added some more utility methods.

--- backend/src/system/IPFS.py
+++ backend/src/system/IPFS.py
@@ -1,14 +1,13 @@
 import pickle
+from datetime import datetime
 
 import requests
 
+from simulation.IPFS import Mode
 from system.IPARO import IPARO
 from system.IPAROException import IPARONotFoundException
 from system.IPAROLink import IPAROLink
 from system.Utils import Utils
-from simulation.IPFS import Mode
-from datetime import datetime
-
 
 
 class IPFS:
@@ -29,7 +28,7 @@ class IPFS:
         iparo = pickle.loads(response.content)
         return iparo
 
-    def retrieve_by_number(self, latest_link: IPAROLink, num: int):
+    def retrieve_by_number(self, latest_link: IPAROLink, num: int) -> tuple[str, IPARO]:
         """Fetch an IPARO with the desired sequence number, using a link to the latest node."""
         curr_link = latest_link
         if num < 0 or num > latest_link.seq_num:
@@ -39,16 +38,23 @@ class IPFS:
             curr_link = min([linked_iparo for linked_iparo in iparo.linked_iparos if linked_iparo.seq_num >= num],
                             key=lambda link: link.seq_num)
 
-        return curr_link
+        iparo = self.retrieve(curr_link.cid)
+        return curr_link.cid, iparo
 
-    def retrieve_by_date(self, latest_link: IPAROLink, target_timestamp: str, mode: Mode):
+    def retrieve_by_date(self, latest_link: IPAROLink, target_timestamp: str, mode: Mode) -> tuple[IPAROLink, IPARO, set[IPAROLink]]:
         """Fetch an IPARO with the closest timestamp, using a link to the latest node. For
         the closest timestamp, if the target timestamp is exactly halfway between the current
-        timestamp and the previous timestamp, then the method will return the current link."""
+        timestamp and the previous timestamp, then the method will return the current link. To
+        get the latest cid for a URL, use the `resolve_cid` method from IPNS. Then, to get the
+        latest link, use the `from_cid_iparo()` method from IPAROLinkFactory. This method will return
+        the CID of the IPARO with the closest date, the corresponding IPARO object, and the known IPAROLinks
+        to use for later access.
+        """
         # Scenario 1: Current link timestamp <= target timestamp
         # Scenario 2: There is no previous node.
         if latest_link.timestamp <= target_timestamp or latest_link.seq_num == 0:
-            return latest_link
+            iparo = self.retrieve(latest_link.cid)
+            return latest_link, iparo, set()
 
         # Otherwise, we assume that a previous node exists and target timestamp < current timestamp.
         curr_link = latest_link
@@ -65,11 +71,9 @@ class IPFS:
         while prev_link.timestamp > target_timestamp:
             # Now we update
             curr_link = min(known_links, key=lambda link: (link.timestamp, link.seq_num))
-            if curr_link.seq_num == 0 and curr_link.timestamp > target_timestamp:
-                return curr_link  # No way to get closer than current link at this point.
-
             iparo = self.retrieve(curr_link.cid)
-            
+            if curr_link.seq_num == 0 and curr_link.timestamp > target_timestamp:
+                return curr_link, iparo, known_links  # No way to get closer than current link at this point.
             # To test prev_link condition.
             candidate_links = {link for link in iparo.linked_iparos if link.timestamp >= target_timestamp}
             known_links.update(candidate_links)
@@ -81,14 +85,36 @@ class IPFS:
 
         # Scenario 3: prev_ts == curr_ts (causing div by 0)
         if prev_ts == curr_ts:
-            return curr_link
+            iparo = self.retrieve(curr_link.cid)
+            return curr_link, iparo, known_links
 
         # Scenario 4: prev_ts < curr_ts
         # Calculate ratio to see if it's closer or not.
         r = (target_ts - curr_ts) / (curr_ts - prev_ts)
         if mode == Mode.CLOSEST:
-            return prev_link if r < 0.5 else curr_link
+            link = prev_link if r < 0.5 else curr_link
         elif mode == Mode.LATEST_BEFORE:
-            return prev_link if r < 1 else curr_link
-        # else (mode == Mode.EARLIEST_AFTER):
-        return prev_link if r == 0 else curr_link
+            link = prev_link if r < 1 else curr_link
+        else:
+            link = prev_link if r == 0 else curr_link
+
+        iparo = self.retrieve(link.cid)
+
+        return link, iparo, known_links
+
+    def retrieve_all_iparos_in_range(self, earliest_cid: str, link: IPAROLink,
+                                     iparo: IPARO) -> dict[str, IPARO]:
+        """
+        Retrieves all IPAROs in range with their CIDs, up to earliest_cid.
+        """
+        snapshots = {link.cid: iparo}
+        # Get all CIDs up to earliest CID.
+        while link.cid != earliest_cid:
+            link = max(iparo.linked_iparos, key=lambda iparo: iparo.seq_num)
+            iparo = self.retrieve(link.cid)
+            snapshots[link.cid] = iparo
+        # Get earliest CID
+        iparo = self.retrieve(earliest_cid)
+        snapshots[earliest_cid] = iparo
+
+        return snapshots
--- backend/src/system/SnapshotsUtils.py
+++ backend/src/system/SnapshotsUtils.py
@@ -1,6 +1,26 @@
+from datetime import datetime, timedelta
+
+from simulation.IPFS import Mode
 from system.IPARO import IPARO
-from system.IPNS import IPNS
+from system.IPAROException import IPARONotFoundException
+from system.IPAROLink import IPAROLink
+from system.IPAROLinkFactory import IPAROLinkFactory
 from system.IPFS import IPFS
+from system.IPNS import IPNS
+
+def check_latest_cid(url: str, ipns: IPNS, ipfs: IPFS, ipns_records: dict) -> tuple[IPAROLink, IPARO]:
+    """Checks the archival status and if it is not archived, throws a ValueError."""
+    peer_id = ipns_records.get(url)
+    if not peer_id:
+        raise ValueError("This website has not been archived")
+
+    # W
+    latest_cid = ipns.resolve_cid(peer_id)
+    latest_iparo = ipfs.retrieve(latest_cid)
+    latest_link = IPAROLinkFactory.from_cid_iparo(latest_cid, latest_iparo)
+
+    return latest_link, latest_iparo
+
 
 def get_all_snapshots_for_url(url: str, ipns: IPNS, ipfs: IPFS, ipns_records: dict) -> dict[str, IPARO]:
     peer_id = ipns_records.get(url)
@@ -25,3 +45,27 @@ def get_all_snapshots_for_url(url: str, ipns: IPNS, ipfs: IPFS, ipns_records: di
     get_all_cids(start_cid)
 
     return dict(sorted(snapshots.items(), key=lambda item: item[1].seq_num))
+
+def retrieve_by_number(url: str, ipns: IPNS, ipfs: IPFS, num: int, ipns_records: dict) -> dict[str, IPARO]:
+    """
+    Returns a JSON object that retrieves an IPARO by sequence number.
+    """
+    latest_link, latest_iparo = check_latest_cid(url, ipns, ipfs, ipns_records)
+    cid, iparo = ipfs.retrieve_by_number(latest_link, num)
+    return {cid: iparo}
+
+
+def retrieve_by_date(url: str, ipns: IPNS, ipfs: IPFS, date: str, ipns_records: dict) -> dict[str, IPARO]:
+    """
+    Returns a JSON object that retrieves all IPARO objects for a specific date.
+    """
+    latest_link, latest_iparo = check_latest_cid(url, ipns, ipfs, ipns_records)
+
+    earliest_time = datetime.strptime("%Y-%m-%d", date)
+    latest_timestamp = (earliest_time + timedelta(days=1, microseconds=-1)).isoformat()
+    earliest_timestamp = earliest_time.isoformat()
+
+    latest_link, latest_iparo, known_links = ipfs.retrieve_by_date(latest_link, latest_timestamp, Mode.LATEST_BEFORE)
+    earliest_link, _, _ = ipfs.retrieve_by_date(latest_link, earliest_timestamp, Mode.EARLIEST_AFTER)
+    iparos = ipfs.retrieve_all_iparos_in_range(earliest_link.cid, latest_link, latest_iparo)
+    return iparos
