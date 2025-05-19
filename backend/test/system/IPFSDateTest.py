6251b4e4937bb3f56d90ddb3cb479bbd08aedb12
Quang Bui <buivuphuquang0311@gmail.com>
2025-05-19 18:29:20 -0400

    IPARO Test

--- backend/test/system/IPFSDateTest.py
+++ backend/test/system/IPFSDateTest.py
@@ -22,7 +22,8 @@ for i in range(100):
                        timestamp=ts.isoformat().replace("+00:00", "Z"),
                        url="http://memento.us/",
                        content=b"Hello",
-                       linked_iparos=linked_iparos)
+                       linked_iparos=linked_iparos
+                       )
     # Mock a hash function
     iparo_hash = hashlib.sha256(pickle.dumps(curr_iparo)).hexdigest()
     cid = "Qm" + iparo_hash[:34]  # Not sure why they chose this number.
@@ -30,14 +31,8 @@ for i in range(100):
     mock_iparos[cid] = curr_iparo
     prev_link = IPAROLinkFactory.from_cid_iparo(cid, curr_iparo)
 
-latest_cid = prev_link.cid
+latest_link = prev_link
 
-
-def test_timestamp(rel_time: timedelta, mode: Mode = Mode.CLOSEST):
-    time = time1 + rel_time
-    time_string = time.isoformat().replace("+00:00", "Z")
-    iparo, _ = ipfs.retrieve_by_date(latest_cid, time_string, mode)
-    return iparo.seq_num
 
 class IPFSDateTest(TestCase):
 
@@ -47,32 +42,52 @@ class IPFSDateTest(TestCase):
         ipfs.retrieve = mock_return
 
     def test_ipfs_should_retrieve_later_time_if_two_closest_timestamps_are_equally_distant(self):
-        seq_num = test_timestamp(timedelta(hours=5, minutes=30))
-        self.assertTrue(seq_num == 5 or seq_num == 6)
+        time = time1 + timedelta(hours=5, minutes=30)
+        time_string = time.isoformat().replace("+00:00", "Z")
+        link, _, _ = ipfs.retrieve_by_date(latest_link, time_string, Mode.CLOSEST)
+        self.assertTrue(link.seq_num == 5 or link.seq_num == 6)
 
     def test_ipfs_should_retrieve_earlier_time_if_closest_timestamp_is_earlier(self):
-        seq_num = test_timestamp(timedelta(hours=5, minutes=29))
-        self.assertEqual(seq_num, 5)
+        time = time1 + timedelta(hours=5, minutes=29)
+        time_string = time.isoformat().replace("+00:00", "Z")
+        link, _, _ = ipfs.retrieve_by_date(latest_link, time_string, Mode.CLOSEST)
+
+        self.assertEqual(link.seq_num, 5)
 
     def test_ipfs_should_retrieve_later_time_if_closest_timestamp_is_later(self):
-        seq_num = test_timestamp(timedelta(hours=5, minutes=31))
-        self.assertEqual(seq_num, 6)
-        
+        time = time1 + timedelta(hours=5, minutes=31)
+        time_string = time.isoformat().replace("+00:00", "Z")
+        link, _, _ = ipfs.retrieve_by_date(latest_link, time_string, Mode.CLOSEST)
+
+        self.assertEqual(link.seq_num, 6)
+
     def test_ipfs_should_retrieve_earliest_time_after_timestamp(self):
-        seq_num = test_timestamp(timedelta(hours=5, minutes=1), Mode.EARLIEST_AFTER)
-        self.assertEqual(seq_num, 6)
+        time = time1 + timedelta(hours=5, minutes=1)
+        time_string = time.isoformat().replace("+00:00", "Z")
+        link, _, _ = ipfs.retrieve_by_date(latest_link, time_string, Mode.EARLIEST_AFTER)
+
+        self.assertEqual(link.seq_num, 6)
 
     def test_ipfs_should_retrieve_latest_time_before_timestamp(self):
-        seq_num = test_timestamp(timedelta(hours=5, minutes=59), Mode.LATEST_BEFORE)
-        self.assertEqual(seq_num, 5)
+        time = time1 + timedelta(hours=5, minutes=59)
+        time_string = time.isoformat().replace("+00:00", "Z")
+        link, _, _ = ipfs.retrieve_by_date(latest_link, time_string, Mode.LATEST_BEFORE)
+
+        self.assertEqual(link.seq_num, 5)
 
     def test_ipfs_should_retrieve_first_node_if_target_timestamp_comes_before(self):
-        seq_num = test_timestamp(timedelta(hours=-5))
-        self.assertEqual(seq_num, 0)
+        time = time1 - timedelta(hours=5)
+        time_string = time.isoformat().replace("+00:00", "Z")
+        link, _, _ = ipfs.retrieve_by_date(latest_link, time_string, Mode.CLOSEST)
+
+        self.assertEqual(link.seq_num, 0)
 
     def test_ipfs_should_retrieve_latest_node_if_target_timestamp_comes_after(self):
-        seq_num = test_timestamp(timedelta(hours=101))
-        self.assertEqual(seq_num, 99)
+        time = time1 + timedelta(hours=101)
+        time_string = time.isoformat().replace("+00:00", "Z")
+        link, _, _ = ipfs.retrieve_by_date(latest_link, time_string, Mode.CLOSEST)
+
+        self.assertEqual(link.seq_num, 99)
 
 
 if __name__ == '__main__':
