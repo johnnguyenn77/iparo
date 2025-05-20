16d85e8c2ef3f1ddf40b8925e953526ae24aada6
Quang Bui <buivuphuquang0311@gmail.com>
2025-05-20 00:21:49 -0400

    Add 10 unit tests to complete coverage for IPARO object

--- backend/test/SysIPAROTestConstant.py
+++ backend/test/SysIPAROTestConstant.py
@@ -1,5 +1,5 @@
-# backend/test/system/IPAROTestConstants.py
-from system.IPARO import IPARO  # assumes system is in PYTHONPATH
+from system.IPARO import IPARO
+from system.IPAROLink import IPAROLink
 
 
 time1 = "2013-02-02T10:00:00Z"
@@ -7,29 +7,34 @@ url1 = "http://memento.us/"
 content_type1 = "application/http; msgtype=response"
 content1 = b"<html><body>Memento for 2/2/2013 10:00am</body></html>"
 cid1 = 1
+seq_num1 = 0
 
 time2 = "2016-12-31T11:00:00Z"
 url2 = "http://memento.us/"
 content_type2 = "application/http; msgtype=response"
 content2 = b"<html><body>Memento for 12/31/2016 11:00am</body></html>"
 cid2 = 2
+links2 = {IPAROLink(1, time1, cid1)}
+seq_num2 = 1
 
 time3 = "2017-12-31T11:00:00Z"
 url3 = "http://someotherURI.us/"
 content_type3 = "application/http; msgtype=response"
 content3 = b"<html><body>SomeotherURI</body></html>"
 cid3 = 3
+seq_num3 = 0
 
 time4 = "2018-12-31T11:00:00Z"
 url4 = "http://anothersite.us/"
 content_type4 = "application/http; msgtype=response"
 content4 = b"<html><body>Another site</body></html>"
 cid4 = 4
+seq_num4 = 0
 
 iparo1 = IPARO(
     url=url1,
     timestamp=time1,
-    seq_num=0,
+    seq_num=seq_num1,
     linked_iparos=frozenset(),
     content_type=content_type1,
     content=content1,
@@ -39,8 +44,8 @@ iparo1 = IPARO(
 iparo2 = IPARO(
     url=url2,
     timestamp=time2,
-    seq_num=1,
-    linked_iparos=frozenset(),  # Fix this when IPAROLink is available
+    seq_num=seq_num2,
+    linked_iparos=links2,
     content_type=content_type2,
     content=content2,
     nonce=0
@@ -49,7 +54,7 @@ iparo2 = IPARO(
 iparo3 = IPARO(
     url=url3,
     timestamp=time3,
-    seq_num=0,
+    seq_num=seq_num3,
     linked_iparos=frozenset(),
     content_type=content_type3,
     content=content3,
@@ -59,7 +64,7 @@ iparo3 = IPARO(
 iparo4 = IPARO(
     url=url4,
     timestamp=time4,
-    seq_num=0,
+    seq_num=seq_num4,
     linked_iparos=frozenset(),
     content_type=content_type4,
     content=content4,
